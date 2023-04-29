d8c611569a7cace1988056aa1013f74ed2a5b5f1
tmt2504 <tmt2504@gmail.com>
2023-04-29 18:16:40 +0700

    add applicationDbContext

---
+++ BookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWeb.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+
+	}
+}
+
--- BookWeb/Program.cs
+++ BookWeb/Program.cs
@@ -1,7 +1,13 @@
+using BookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add connection string
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
