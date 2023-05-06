204239e8f18cb46c4403563e17f405e8c85a0893
tmt2504 <tmt2504@gmail.com>
2023-05-06 09:48:00 +0700

    create category list view

--- BookWeb/Controllers/CategoryController.cs
+++ BookWeb/Controllers/CategoryController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookWeb.Data;
+using BookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +12,16 @@ namespace BookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Category> categoryList = _db.Category.ToList();
+            return View(categoryList);
         }
     }
 }
